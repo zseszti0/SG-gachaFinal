869f9ec3a26604b06f41d48b40edebacd6757500
zseszti0 <zseszti42.3@gmail.com>
2024-02-14 14:12:11 +0100

    v

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,8 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs'
+			pages: 'build',
+			assets: 'build'
 		}),
 		
 		
