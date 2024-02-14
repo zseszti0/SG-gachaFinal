30bcdb3f3db97b019dce53294caed5abee271d37
zseszti0 <zseszti42.3@gmail.com>
2024-02-14 12:43:18 +0100

    dpokvsk

---
+++ docs/_app/immutable/chunks/cardV2BG.Sxb0NtTQ.js
@@ -0,0 +1,1 @@
+const a=""+new URL("../assets/cardV2BG.qJaF3-hT.png",import.meta.url).href;export{a as c};
---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
+		
+		
+	}
+};
+
+export default config;
